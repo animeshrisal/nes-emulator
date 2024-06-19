0d97e6e00175297173f4b2964943704f2ad16747
Animesh Risal <animeshrisal99@gmail.com>
2024-06-19 15:33:01 +0200

    Add read/write function for Memory

--- bus.c
+++ bus.c
@@ -3,7 +3,12 @@
 #include "./bus.h"
 
 void write_to_bus(Bus *bus, uint16_t addr, uint8_t data) {
-  if(addr >= 0x0000 && addr <= 0xFFFF) {
-    bus->memory = addr
-  }
-}``
+  if (addr >= 0x0000 && addr <= 0xFFFF) {
+   bus->memory[addr] = data;
+  };
+}
+
+uint8_t read_from_bus(Bus *bus, uint8_t addr) {
+  return bus->memory[addr];
+}
+
