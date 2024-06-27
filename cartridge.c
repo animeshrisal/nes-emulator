e816db62c0611adacfb03a38c5cd02e9614c1afa
Animesh Risal <animeshrisal99@gmail.com>
2024-06-27 22:44:12 +0200

    Fix memory allocation which caused it to crash

--- cartridge.c
+++ cartridge.c
@@ -23,13 +23,12 @@ void read_from_cartridge(Bus *bus, Cartridge *cartridge) {
 
   uint16_t prg_rom = nesHeader.prg_rom * PRG_ROM_SIZE;
   uint16_t chr_rom = nesHeader.chr_rom * CHR_ROM_SIZE;
+
   printf("0x%02x\n", nesHeader.mapper1);
   printf("%d\n", nesHeader.mapper2);
   cartridge->cartridgeHeader = nesHeader;
-  cartridge->prgRomSize =
-      (uint8_t *)malloc(nesHeader.prg_rom * sizeof(uint8_t));
-  cartridge->chrRomSize =
-      (uint8_t *)malloc(nesHeader.prg_rom * sizeof(uint8_t));
+  cartridge->prgRomSize = malloc(prg_rom);
+  cartridge->chrRomSize = malloc(chr_rom);
 
   if (fread(cartridge->prgRomSize, prg_rom, 1, file) != 1) {
     perror("Error reading prg!");
@@ -39,9 +38,11 @@ void read_from_cartridge(Bus *bus, Cartridge *cartridge) {
     perror("Error reading chr!");
   }
 
-  for (int i = 0; i < 16; i++) {
+  for (int i = 0; i < prg_rom + 1; i++) {
     printf("0x%02X\n", cartridge->prgRomSize[i]);
   }
+
+  fclose(file);
 
   /*
   for (int i = 0; i < chr_rom; i++) {
